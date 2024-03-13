a7f88279e605b0023016f95b90f8f5b3a7711673
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-03-13 15:41:13 +0100

    mock backend initial implementation

---
+++ src/app/kanban-board/task-backend.service.ts
@@ -0,0 +1,105 @@
+import { Injectable, inject } from '@angular/core';
+import { nanoid } from 'nanoid';
+import { LoadingService } from '../loading.service';
+import { Task } from '../task';
+import { z } from 'zod';
+
+const taskSchema = z.object({
+  id: z.string(),
+  description: z.string(),
+  status: z.enum(['todo', 'doing', 'done']),
+});
+
+const tasksSchema = z.array(taskSchema);
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskBackendService {
+  private readonly loadingService = inject(LoadingService);
+  private readonly MOCK_SERVER_RESONPOSE_TIME_MS = 500;
+  private readonly INITIAL_MOCK_TASKS = [
+    {
+      id: '1',
+      description: 'Get to work',
+      status: 'todo',
+    },
+    {
+      id: '2',
+      description: 'Pick up groceries',
+      status: 'todo',
+    },
+    {
+      id: '3',
+      description: 'Go home',
+      status: 'todo',
+    },
+    {
+      id: '4',
+      description: 'Fall asleep',
+      status: 'todo',
+    },
+    {
+      id: '5',
+      description: 'Get up',
+      status: 'doing',
+    },
+    {
+      id: '6',
+      description: 'Brush teeth',
+      status: 'doing',
+    },
+    {
+      id: '7',
+      description: 'Take a shower',
+      status: 'done',
+    },
+    {
+      id: '8',
+      description: 'Check e-mail',
+      status: 'done',
+    },
+    {
+      id: '9',
+      description: 'Walk dog',
+      status: 'done',
+    },
+  ];
+
+  fetchTasks(): Promise<Task[]> {
+    return new Promise((resolve, reject) => {
+      this.loadingService.show();
+
+      setTimeout(() => {
+        try {
+          resolve(tasksSchema.parse(this.INITIAL_MOCK_TASKS));
+        } catch (error) {
+          reject(error);
+        } finally {
+          this.loadingService.hide();
+        }
+      }, this.MOCK_SERVER_RESONPOSE_TIME_MS * 3);
+    });
+  }
+
+  createTask(task: Omit<Task, 'id'>): Promise<Task> {
+    return new Promise((resolve, reject) => {
+      this.loadingService.show();
+
+      setTimeout(() => {
+        try {
+          resolve(
+            taskSchema.parse({
+              id: nanoid(),
+              ...task,
+            })
+          );
+        } catch (error) {
+          reject(error);
+        } finally {
+          this.loadingService.hide();
+        }
+      }, this.MOCK_SERVER_RESONPOSE_TIME_MS);
+    });
+  }
+}
