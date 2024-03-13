36b34334fc29e0bbaac599cea06486283869bc68
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-03-13 15:36:39 +0100

    Created a component for individual tasks

---
+++ src/app/kanban-board/task-group/task-card/task-card.component.ts
@@ -0,0 +1,25 @@
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Task } from '../../../task';
+
+@Component({
+  selector: 'app-task-card',
+  standalone: true,
+  imports: [CdkDrag, CdkDropList, MatCardModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <mat-card class="dark:!bg-neutral-600 !bg-neutral-100 rounded">
+      <mat-card-header>
+        <!-- TODO: Add buttons to change task status, and to delete them -->
+      </mat-card-header>
+      <mat-card-content>
+        {{ task().description }}
+      </mat-card-content>
+    </mat-card>
+  `,
+  styles: ``,
+})
+export class TaskCardComponent {
+  task = input.required<Task>();
+}
