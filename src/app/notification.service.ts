c360bc928090935a1afe32470ba937b582842104
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-03-13 15:34:30 +0100

    created a notification service, so the user can receive feedback on errors

---
+++ src/app/notification.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  snackBar = inject(MatSnackBar);
+
+  showSuccessNotification(message: string) {
+    this.snackBar.open(message, 'OK', {
+      duration: 3000,
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+    });
+  }
+
+  showErrorNotification(message: string) {
+    this.snackBar.open(message, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+    });
+  }
+}
