622a2fdf4910f9d189845d5b1eb005f365fe2f2a
Gergely Kovacs <gergely-kovacs@hotmail.com>
2024-03-13 15:40:11 +0100

    added a drag and drop container for task cards

---
+++ src/app/kanban-board/task-group/task-group.component.ts
@@ -0,0 +1,34 @@
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Task } from '../../task';
+import { TaskCardComponent } from './task-card/task-card.component';
+
+@Component({
+  selector: 'app-task-group',
+  standalone: true,
+  imports: [MatCardModule, CdkDrag, TaskCardComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <mat-card>
+      <mat-card-header class="{{ headerClass() }}">
+        <mat-card-title class="text-gray-50">{{ title() }}</mat-card-title>
+      </mat-card-header>
+      <mat-card-content class="!pb-0">
+        @for (task of tasks(); track task.id) {
+        <app-task-card
+          class="cursor-move m-4"
+          cdkDrag
+          [task]="task"
+        ></app-task-card>
+        }
+      </mat-card-content>
+    </mat-card>
+  `,
+  styles: ``,
+})
+export class TaskGroupComponent {
+  headerClass = input.required<string>();
+  title = input.required<string>();
+  tasks = input.required<Task[]>();
+}
